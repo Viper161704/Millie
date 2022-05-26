9860a7c223c56ec78360473c7b7cdf31603f5d69
Adarsh <103887234+Viper161704@users.noreply.github.com>
2022-05-26 17:27:43 +0530

    Update _firstchat.js

--- plugins/_firstchat.js
+++ plugins/_firstchat.js
@@ -10,7 +10,7 @@ handler.all = async function (m) {
     if (new Date - user.pc < 86400000) return // setiap 24 jam sekali
     await this.sendButton(m.chat, `
 Hai,
- 'I am ᴍɪʟʟɪᴇ, please dont spam,call,ask to save to this number. Can I help you?, Type .menu To Get Bot Menu'
+ 'I am νιρєя, please dont spam,call,ask to save to this number. Can I help you?, Type .menu To Get Bot Menu'
 `.trim(), watermark, user.banned ? 'Bot Owner' : 'Menu',`.menu`,'.owner' ,`.owner`, m)
     user.pc = new Date * 1
 }

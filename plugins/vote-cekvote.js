8443e67de6d5332604587515cd6e02ade770f6b8
Adarsh <103887234+Viper161704@users.noreply.github.com>
2022-05-27 10:53:48 +0530

    Update vote-cekvote.js

--- plugins/vote-cekvote.js
+++ plugins/vote-cekvote.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, usedPrefix }) => {
     let id = m.chat
     conn.vote = conn.vote ? conn.vote : {}
     if (!(id in conn.vote)) {
-        await conn.sendButton(m.chat, `_*no voting in this group!*_`, '© Ammu', 'START VOTE', `${usedPrefix}startvote`)
+        await conn.sendButton(m.chat, `_*no voting in this group!*_`, '© 𝛻𝛪𝛲𝛯𝑅 𝑆𝛯𝑅', 'START VOTE', `${usedPrefix}startvote`)
         throw false
     }
 
